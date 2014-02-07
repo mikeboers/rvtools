6db988455598a7f613059281245418eb44bc80af
Mike Boers <github@mikeboers.com>
2014-02-07 13:56:05 -0800

    Test that the new and old writers match

--- tests/test_gto_writer.py
+++ tests/test_gto_writer.py
@@ -27,25 +27,45 @@ class TestGTOWriting(TestCase):
 
 class TestOldWriter(TestCase):
 
-    def test_main(self):
+    def test_old(self):
 
         fh = StringIO()
         gto = Writer(fh)
 
         with gto.object('Object'):
             with gto.component('Component'):
-                gto.property('string_prop', "hello")
                 gto.property('float_prop', 3.14)
                 gto.property('int_array_prop', [[1, 2, 3, 4]])
+                gto.property('string_prop', "hello")
 
         self.assertSimilarStrings(fh.getvalue(), '''
             GTOa (3)
             Object {
                 Component {
-                    string string_prop = "hello"
                     float float_prop = 3.14
                     int[4] int_array_prop = [ [ 1 2 3 4 ] ]
+                    string string_prop = "hello"
                 }
             }
         ''')
 
+    def test_new(self):
+
+        gto = GTO(3, Object=dict(
+            Component=dict(
+                string_prop='hello',
+                float_prop=3.14,
+                int_array_prop=[[1, 2, 3, 4]],
+            )
+        ))
+
+        self.assertSimilarStrings(gto.dumps(), '''
+            GTOa (3)
+            Object {
+                Component {
+                    float float_prop = 3.14
+                    int[4] int_array_prop = [ [ 1 2 3 4 ] ]
+                    string string_prop = "hello"
+                }
+            }
+        ''')
