21b2018d18ae5418ca20f56708fb12c51479a3cc
Mike Boers <github@mikeboers.com>
2014-02-07 14:19:11 -0800

    Replace the guts of the old with the new

--- rvtools/gto/writer.py
+++ rvtools/gto/writer.py
@@ -1,37 +1,22 @@
 import contextlib
 
-from .core import gto_repr, gto_type
+from .core import GTO, Object, Component, Property
 
 
 class Writer(object):
 
     def __init__(self, fh):
         self.fh = fh
-        self._indent = 0
-        self.writeline('GTOa (3)')
+        self.fh.write('GTOa (3)\n')
+        self.gto = GTO()
+        self.stack = []
 
-    def indent(self, amount=1):
-        self._indent += amount
-
-    def dedent(self, amount=1):
-        self._indent -= amount
-
-    def writeline(self, line):
-        self.fh.write('%s%s\n' % ('\t' * self._indent, line))
-
-    def begin_object(self, name, protocol='object', version=0):
-        name = gto_repr(name, True)
-        if protocol != 'object':
-            name = '%s : %s' % (name, protocol)
-        if version:
-            name = '%s (%d)' % (name, version)
-        self.writeline(name)
-        self.writeline('{')
-        self.indent()
+    def begin_object(self, *args, **kwargs):
+        self.stack.append(Object(*args, **kwargs))
 
     def end_object(self):
-        self.dedent()
-        self.writeline('}')
+        obj = self.stack.pop()
+        self.fh.write(''.join(obj.iter_dumps()))
 
     @contextlib.contextmanager
     def object(self, *args, **kwargs):
@@ -41,16 +26,13 @@ class Writer(object):
         finally:
             self.end_object()
 
-    def begin_component(self, name, interpretation=None):
-        if interpretation:
-            line = '%s as %s' % (gto_repr(name, True), gto_repr(interpretation))
-        else:
-            line = gto_repr(name, True)
-        self.writeline(line)
-        self.writeline('{')
-        self.indent()
+    def begin_component(self, *args, **kwargs):
+        comp = Component(*args, **kwargs)
+        self.stack[-1].add(comp)
+        self.stack.append(comp)
 
-    end_component = end_object
+    def end_component(self):
+        self.stack.pop()
 
     @contextlib.contextmanager
     def component(self, *args, **kwargs):
@@ -60,17 +42,6 @@ class Writer(object):
         finally:
             self.end_component()
 
-    def property(self, name, value, type=None, basetype=None, interpretation=None):
-
-        type = type or gto_type(value, basetype)
-        if interpretation:
-            line = '%s %s as %s = %s' % (type, gto_repr(name, True), interpretation, gto_repr(value))
-        else:
-            line = '%s %s = %s' % (type, gto_repr(name, True), gto_repr(value))
-
-        self.writeline(line)
-
-
-
-
+    def property(self, *args, **kwargs):
+        self.stack[-1].add(Property(*args, **kwargs))
 
